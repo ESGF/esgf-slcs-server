acbe4ca393d8f7917768f155f08482ebe56613ef
Matt Pryor <matt.pryor@stfc.ac.uk>
2018-06-16 21:30:09 +0100

    Revamp to work with django-onlineca

--- esgf_slcs/views.py
+++ esgf_slcs/views.py
@@ -10,52 +10,20 @@ import functools
 
 from django.conf import settings
 from django.views.decorators.csrf import csrf_exempt
-from django.views.decorators.http import require_safe, require_POST
+from django.views.decorators.http import require_POST
 from django.utils.decorators import decorator_from_middleware
-from django.http import HttpResponse, HttpResponseForbidden
+from django.http import HttpResponse
 from django.contrib.auth import authenticate
 
-from paste.deploy import loadapp
-from django_wsgi.embedded_wsgi import call_wsgi_app
 from oauth2_provider.decorators import protected_resource
 from oauth2_provider.middleware import OAuth2TokenMiddleware
 
-
-# Use Paste Deploy to create the online ca app
-onlineca_app = loadapp(settings.ONLINECA_PASTEDEPLOY_CONF)
-
-
-@require_safe
-def trustroots(request):
-    """
-    Django view that calls the OnlineCA WSGI app with ``/trustroots/`` as the
-    path info.
-    """
-    return call_wsgi_app(onlineca_app, request, '/trustroots/')
-
-
-def certificate(request):
-    """
-    Django view that calls the OnlineCA WSGI app with ``/certificate/`` as the
-    path info and the logged in user's openid in the environ.
-
-    There must be a logged in user by some mechanism.
-    """
-    # The user must be authenticated
-    if not request.user.is_authenticated():
-        return HttpResponseForbidden()
-    # Convert the user to an OpenID using the specified function
-    if callable(settings.ONLINECA_DJANGO_USER_TO_OPENID):
-        converter = settings.ONLINECA_DJANGO_USER_TO_OPENID
-    else:
-        from django.utils.module_loading import import_string
-        converter = import_string(settings.ONLINECA_DJANGO_USER_TO_OPENID)
-    request.environ['OPENID'] = converter(request.user)
-    return call_wsgi_app(onlineca_app, request, '/certificate/')
+from onlineca import views as onlineca_views
 
 
 #: Decorator that checks the request for a valid OAuth token
 require_oauth_token = decorator_from_middleware(OAuth2TokenMiddleware)
+
 
 @protected_resource(scopes = [settings.CERTIFICATE_SCOPE])
 @require_oauth_token
@@ -65,7 +33,7 @@ def certificate_oauth(request):
     """
     Certificate view that is protected by OAuth login.
     """
-    return certificate(request)
+    return onlineca_views.certificate(request)
 
 
 def require_http_basic_auth(view):
@@ -86,11 +54,11 @@ def require_http_basic_auth(view):
                     request.user = user
                     return view(request, *args, **kwargs)
         # If auth failed, send back a 401 with a challenge
-        response = HttpResponse()
-        response.status_code = 401
+        response = HttpResponse(status = 401)
         response['WWW-Authenticate'] = 'Basic realm="%s"' % settings.BASIC_AUTH_REALM
         return response
     return _decorated
+
 
 @require_http_basic_auth
 @require_POST
@@ -99,4 +67,4 @@ def certificate_basicauth(request):
     """
     Certificate view that is protected by Basic Auth login.
     """
-    return certificate(request)
+    return onlineca_views.certificate(request)
--- esgf_slcs_server/wsgi.py
+++ esgf_slcs_server/wsgi.py
@@ -3,17 +3,12 @@
 WSGI config for the ESGF SLCS Server.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
 
 import os
 
-import django
-import django_wsgi.handler
+from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "esgf_slcs_server.settings")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'esgf_slcs_server.settings')
 
-django.setup()
-application = django_wsgi.handler.APPLICATION
+application = get_wsgi_application()
